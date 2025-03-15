63a62b556218421b294e23c949c710de658b6a45
lndsilva <lndsilva@hotmail.com>
2025-03-15 19:56:52 -0300

    CRUD para imagens no BD MySQL

---
+++ getImagem.php
@@ -0,0 +1,13 @@
+<?php
+	$host = "localhost";
+	$username = "root";
+	$password = "@123Abc@";
+	$db = "dbPessoa";
+	$PicNum = $_GET["PicNum"];
+
+	mysql_connect($host,$username,$password) or die("Impossível conectar ao banco.");
+	@mysql_select_db($db) or die("Impossível conectar ao banco.");
+	$result=mysql_query("SELECT * FROM PESSOA WHERE PES_ID=$PicNum") or die("Impossível executar a query ");
+	$row=mysql_fetch_object($result);
+	Header( "Content-type: image/gif");
+	echo $row->PE
